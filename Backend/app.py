117784666ce79be45ce6b04b254a90c164ddc15c
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-10-25 15:22:44 +0530

    For Backend

---
+++ Backend/app.py
@@ -0,0 +1,17 @@
+#this will the main file for backend
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_jwt_extended import JWTManager, jwt_required, current
+from config import Config
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
