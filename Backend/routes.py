eb021eac9575978ffdc6e8982f7429e05c3cb7a0
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-10-26 19:38:49 +0530

    Changes in Backend

--- Backend/routes.py
+++ Backend/routes.py
@@ -8,5 +8,21 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from models import User , CurrencyConversion
 from app import db
 
+auth_bp = Blueprint('auth', __name__)
+
+
+@auth_bp.route('/register', methods = ['POST']) #these are the decorator functions
+def signup():
+    data = request.get_json()
+    username = data.get(username)
+    password = data.get(password)
+    
+    #check for the user password authentication
+    user = User.query.filer_by(username = username).first()
+    if not user or user.check_password_hash(user.password,password):
+        return jsonify({'message': 'Password Incorrect'}), 401 
+        #401 is the status code for unauthorized access
+
+
 
 
