c36875142c2a2d9651ea7dcf7bda24b45296b4a4
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-10-26 18:57:51 +0530

    Changes in Backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,19 +1,16 @@
 #this will the main file for backend
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
-from flask_jwt_extended import JWTManager, jwt_required, current
-from config import Config
+# from flask_sqlalchemy import SQLAlchemy 
+# from flask_jwt_extended import JWTManager, jwt_required, current
+# from config import Config
+
+app = Flask(__name__)
 
 
-
-
-
-
-
-
-def main():
-    pass
+@app.route('/')
+def index():
+    return 'hi'
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
--- Backend/routes.py
+++ Backend/routes.py
@@ -3,7 +3,10 @@
 #2. One for signin
 #3. One for Currency Conversion
 
-
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from models import User , CurrencyConversion
+from app import db
 
 
 
