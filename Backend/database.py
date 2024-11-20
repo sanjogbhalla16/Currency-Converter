55eb2262f12ae4b5efd0b83866926f1d468d4215
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-11-20 23:18:55 +0530

    changes

--- Backend/database.py
+++ Backend/database.py
@@ -4,9 +4,10 @@ from config import Config
 #using pymongo we are creating a connection to the database
 #and taking the URL from config file
 #db = client["ytmanager"] same same 
-client = MongoClient(Config.MONGO_URI)
+client = MongoClient(Config.MONGO_URI) #instance cluster 
 
-db = client["currency_db"]
+db = client["currency_db"] #collection 
 user_collection = db["users"]
+book_collection = db["books"]
 
 #print(user_collection)
